5c8eb66f03c62a5fd01f3037c7c7d31d106f2ca1
Tarsísio Xavier <tarsisio.xavier@convenia.com.br>
2023-12-20 14:19:36 -0300

    feat(facade): adding assert functions into docblock.

--- src/Support/Facade/Pigeon.php
+++ src/Support/Facade/Pigeon.php
@@ -8,12 +8,17 @@ use Illuminate\Support\Facades\Facade;
 /**
  * Class Pigeon.
  *
- * @method static \Convenia\Pigeon\Drivers\Driver driver(string $driver)
- * @method static \Convenia\Pigeon\Contracts\Publisher routing(string $name = null)
- * @method static \Convenia\Pigeon\Contracts\Publisher exchange(string $name, string $type = 'direct')
- * @method static \Convenia\Pigeon\Contracts\Publisher dispatch(string $eventName, array $event, array $meta = [])
- * @method static \Convenia\Pigeon\Contracts\Consumer queue(string $name, array $properties = [])
  * @method static \Convenia\Pigeon\Contracts\Consumer events(string $name = '#')
+ * @method static \Convenia\Pigeon\Contracts\Consumer queue(string $name, array $properties = [])
+ * @method static \Convenia\Pigeon\Contracts\Publisher dispatch(string $eventName, array $event, array $meta = [])
+ * @method static \Convenia\Pigeon\Contracts\Publisher exchange(string $name, string $type = 'direct')
+ * @method static \Convenia\Pigeon\Contracts\Publisher routing(string $name = null)
+ * @method static \Convenia\Pigeon\Drivers\Driver driver(string $driver)
+ * @method static void assertConsuming(string $queue, int $timeout = null, bool $multiple = null)
+ * @method static void assertConsumingEvent(string $event, int $timeout = null, bool $multiple = null)
+ * @method static void assertDispatched(string $category, array $data)
+ * @method static void assertNotDispatched(string $category, array $data)
+ * @method static void assertPublished(string $routing, array $message)
  */
 class Pigeon extends Facade
 {
